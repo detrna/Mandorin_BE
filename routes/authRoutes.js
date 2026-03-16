f59539dbad67ad656a0584b1e537908adaa55b4c
detrna <himselfunderscore@gmail.com>
2026-03-17 06:46:13 +0700

    fix: Added authenticate middleware to refresh route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ import { authController } from "../controllers/authController.js";
 import { uploadHelper } from "../middleware/uploadHelper.js";
 import { validate } from "../middleware/validate.js";
 import { authSchema } from "../validator/authSchema.js";
+import authenticate from "../middleware/authenticate.js";
 
 const router = Router();
 
@@ -18,7 +19,7 @@ router.post(
   validate(authSchema.registerForeman),
   authController.registerForeman,
 );
-router.post("/refresh", authController.refresh);
+router.post("/refresh", authenticate, authController.refresh);
 router.post("/login", validate(authSchema.login), authController.login);
 
 export default router;
