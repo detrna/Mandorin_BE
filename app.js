f04440b15bf0aa2ce3d6510b203f7b8dde34c0b0
detrna <himselfunderscore@gmail.com>
2026-03-27 16:57:55 +0700

    refactor: routes adjustmnet

--- app.js
+++ app.js
@@ -3,41 +3,16 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { jwtHelper } from "./src/utility/jwtHelper.js";
-
-import authRoutes from "./src/routes/authRoutes.js";
-import clientRoutes from "./src/routes/clientRoutes.js";
-import foremanRoutes from "./src/routes/foremanRoutes.js";
-import reviewRoutes from "./src/routes/reviewRoutes.js";
-import appointmentRoutes from "./src/routes/appointmentRoutes.js";
-import proposalRoutes from "./src/routes/proposalRoutes.js";
-import projectRoutes from "./src/routes/projectRoutes.js";
-import milestoneRoutes from "./src/routes/milestoneRoutes.js";
-import revisionRoutes from "./src/routes/reviewRoutes.js";
-import reportRoutes from "./src/routes/reportRoutes.js";
+import routes from "./src/routes/index.js";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3001",
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
 const port = process.env.PORT;
-const prefix = "/api";
 
-app.use(`${prefix}/auth`, authRoutes);
-app.use(`${prefix}/clients`, clientRoutes);
-app.use(`${prefix}/foreman`, foremanRoutes);
-app.use(`${prefix}/reviews`, reviewRoutes);
-app.use(`${prefix}/appointments`, appointmentRoutes);
-app.use(`${prefix}/proposals`, proposalRoutes);
-app.use(`${prefix}/projects`, projectRoutes);
-app.use(`${prefix}/milestones`, milestoneRoutes);
-app.use(`${prefix}/reports`, reportRoutes);
-app.use(`${prefix}/revisions`, revisionRoutes);
+app.use("/api", routes);
 
 app.get("/cookie", (req, res) => {
   const refresh = req.cookies.refreshToken;
---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import authRoutes from "./authRoutes.js";
+import clientRoutes from "./clientRoutes.js";
+import foremanRoutes from "./foremanRoutes.js";
+import reviewRoutes from "./reviewRoutes.js";
+import appointmentRoutes from "./appointmentRoutes.js";
+import proposalRoutes from "./proposalRoutes.js";
+import projectRoutes from "./projectRoutes.js";
+import milestoneRoutes from "./milestoneRoutes.js";
+import revisionRoutes from "./reviewRoutes.js";
+import reportRoutes from "./reportRoutes.js";
+
+const router = Router();
+
+router.use(`/auth`, authRoutes);
+router.use(`/clients`, clientRoutes);
+router.use(`/foreman`, foremanRoutes);
+router.use(`/reviews`, reviewRoutes);
+router.use(`/routerointments`, appointmentRoutes);
+router.use(`/proposals`, proposalRoutes);
+router.use(`/projects`, projectRoutes);
+router.use(`/milestones`, milestoneRoutes);
+router.use(`/reports`, reportRoutes);
+router.use(`/revisions`, revisionRoutes);
+
+export default router;
