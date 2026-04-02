d5630bde7a209a501fe8402b2ef18503d4646977
detrna <himselfunderscore@gmail.com>
2026-04-02 12:39:39 +0700

    fix: prisma config path fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "src/prisma/schema.prisma",
   migrations: {
-    path: "src/prisma/migrations",
+    path: "prisma/migrations",
   },
 });
